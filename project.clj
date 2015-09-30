0ce3e8e6b85fc3edde4134093d29a68b96fc0a8c
Charles Shinaver <bshinaver@gmail.com>
2015-09-30 17:37:30 -0400

    Added clj-http as plugin dependency

--- project.clj
+++ project.clj
@@ -3,7 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [
+                 [org.clojure/clojure "1.6.0"]
+                 [clj-http "2.0.0"]
+                 ]
   :main ^:skip-aot clojo.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
