45a4994af1a9fe1c5b9725e6fba173174f2fb9d9
Charles Shinaver <bshinaver@gmail.com>
2015-11-04 14:24:39 -0500

    Added usage (no passed command line args)

--- src/clojo/core.clj
+++ src/clojo/core.clj
@@ -5,7 +5,19 @@
     [clojo.zoho :as zoho])
   )
 
+(defn parse-args [& args]
 (defn -main
+  (let [n (count args)]
+    (cond
+      (= n 1) (println (str
+                         "usage: java -jar clojo.jar "
+                         "[--status] "
+                         "[--start-timer] "
+                         "[--stop-timer] "))
+      :else n
+      ))
+  )
+
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
