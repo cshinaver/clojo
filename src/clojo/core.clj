e9f419abf4af6d3ad0023810aa7c671f55f3933f
cshinaver <bshinaver@gmail.com>
2015-11-06 10:50:10 -0500

    Saving auth_token works now

--- src/clojo/core.clj
+++ src/clojo/core.clj
@@ -8,29 +8,28 @@
 (def user-preference-file (str (System/getProperty "user.home") "/" ".clojo"))
 
 (defn- generate-new-auth-token []
-  (println "Authentication token not saved")
   (println "Enter Zoho email address and password")
   (let
-      ;[email (read-line) password (read-line)]
-   [email "test" password "pass"]
+   [email (read-line) password (read-line)]
     (println "Generating new authentication token")
     (let
-        ;[auth_token (zoho/generate-authentication-token email password)]
-     [auth_token (str email "wut" password)]
-      (println (str "Auth token " auth_token " generated"))
-      (println (str "Saving auth token to " user-preference-file))
-      (spit user-preference-file auth_token)
-      auth_token)))
+     [auth_token (zoho/generate-authentication-token email password)]
+      (if-not (nil? auth_token)
+        (do  (println (str "Auth token " auth_token " generated"))
+             auth_token)
+        (println "Failure in generating auth token")))))
 
 (defn- get-auth-token-from-file []
   (slurp user-preference-file))
 
 (defn- get-auth-token []
   (if-not (.exists (io/as-file user-preference-file))
-    (generate-new-auth-token)
-    (if (nil? (resolve 'auth_token))
-      (get-auth-token-from-file)
-      "lol")))
+    (let [auth_token (generate-new-auth-token)]
+      (println "Authentication token not saved")
+      (println (str "Saving auth token to " user-preference-file))
+      (spit user-preference-file auth_token)
+      auth_token)
+    (slurp user-preference-file)))
 
 (defn- non-trivial-parse-args [arg]
   (cond
